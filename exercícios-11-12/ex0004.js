1a530d7677fba89339fab945796a59ae28c506c1
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2022-09-26 17:59:57 -0300

    Create ex0004.js

---
+++ exercícios-11-12/ex0004.js
@@ -0,0 +1,10 @@
+var idade = 
+if (idade < 16){
+    console.log('Não consegue votar no Brasil')
+} else{
+    if (idade > 18 && idade < 65){
+        console.log('Voto obrigatório')
+    }else{
+        console.log('Voto Opicional')
+    }
+}
