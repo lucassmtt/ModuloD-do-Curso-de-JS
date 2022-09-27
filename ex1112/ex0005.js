d1fb404a8f2e3765bc5f2084fb06692ff0640357
Lucas Motta <mottadefreitaslucasgabriel2004@gmail.com>
2022-09-27 14:45:06 -0300

    voto

---
+++ ex1112/ex0005.js
@@ -0,0 +1,9 @@
+var now = new Date()
+var hora = now.getDate
+if (horas > 6 && horas <= 12){
+    console.log('Bom dia')}
+    else if (horas > 12 && horas < 18){
+        console.log('Boa tarde')}
+        else{
+            console.log('Boa noite')
+        }
---
+++ ex1112/ex0006.js
@@ -0,0 +1,25 @@
+var hoje = new Date()
+var dia = hoje.getDay()
+switch (dia){
+    case 0:
+        console.log('Já é Domingo dnv pqp')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('Terça')
+        break
+    case 3:
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('Tamo quase, Quinta')
+        break
+    case 5:
+        console.log('VAMO DALE QUE HOJE É SEXTA')
+        break
+    case 6:
+        console.log('DALEEEE, SÁBADO')
+        break
+}
